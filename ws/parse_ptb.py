174aacd6d32a452ae6fb3df230886a40a9e9ccee
Tue Jensen <tuebjensen@gmail.com>
2024-11-13 00:40:31 +0100

    Added filtering that actually works (I hope) and changed some things so that it works better

--- ws/parse_ptb.py
+++ ws/parse_ptb.py
@@ -6,17 +6,19 @@ from tqdm import tqdm
 import pickle
 import neurokit2 as nk
 import os
+import scipy.signal
 
+def apply_highpass_filter(data, lowcut=0.05, sampling_rate=100, axis=-1):
+    b, a = scipy.signal.butter(5, lowcut, btype="highpass", fs=sampling_rate)
+    return scipy.signal.filtfilt(b, a, data, axis=axis)
+
+# Data is of shape (n_batch, n_samples, n_channels)
 def preprocess(data, sampling_rate=100):
-    # Data is of shape (n_batch, n_samples, n_channels)
-    # Filter the data
-    for i in range(data.shape[0]):
-        # Clean each channel of the data
-        for j in range(data.shape[-1]):
-            data[i, :, j] = nk.ecg_clean(data[i, :, j], sampling_rate=sampling_rate, method="neurokit")
+    # Filter the data. Commonly done with bandpass filter from 0.05 Hz to 150 Hz, but here we only use highpass filter because of sampling rate
+    data_filtered = apply_highpass_filter(data, lowcut=0.05,  sampling_rate=sampling_rate, axis=1)
     # Normalize the data to have zero mean and unit variance along the time axis
-    data = (data - np.mean(data, axis=1)) / np.std(data, axis=1)
-    return data
+    #data_normalized = (data_filtered - np.mean(data_filtered, axis=1)) / np.std(data_filtered, axis=1)
+    return data_filtered
 
 def load_raw_data(df, sampling_rate, path):
     if sampling_rate == 100:
@@ -28,37 +30,38 @@ def load_raw_data(df, sampling_rate, path):
 
 def load_encoder(path = None):
     encoder = TS2Vec(input_dims=12, device="cuda")
-    if path is not None and os.path.exists(path):
+    if path is not None:
+        assert os.path.exists(path), "Encoder model path does not exist"
         encoder.load(path)
     return encoder
 
-# Load the encoder from file if supplied
-encoder = load_encoder(path="results/encoder.pt")
+# Load the encoder from file if supplied and the model exists
+encoder = load_encoder(path=None)
 sampling_rate = 100
 # Number of samples to process at once
 n_samples = 100
-data_path = "data/ptb-xl/"
-out_path = "data/ptb-xl/"
+data_folder = "data/ptb-xl/"
+out_folder = "data/ptb-xl/"
 
 test_fold = 10
-ptb_df = pd.read_csv(data_path+'ptbxl_database_translated.csv')
+ptb_df = pd.read_csv(data_folder+'ptbxl_database_translated.csv')
 ptb_df_train = ptb_df[ptb_df.strat_fold < test_fold]
+ptb_df_train = ptb_df_train
 ptb_df_test = ptb_df[ptb_df.strat_fold == test_fold]
+ptb_df_test = ptb_df_test
 
 # Encodes the data row by row and saves the embeddings and reports in a pickle file
 
 for out_name, dataset in [("parsed_ptb_train.pkl", ptb_df_train), ("parsed_ptb_test.pkl", ptb_df_test)]:
     embedding_report_list = []
     for i in tqdm(range(len(dataset))):
-        row = dataset[i:i+1]
-        data = load_raw_data(row, sampling_rate, data_path)
-        #print(data[0,:,0].shape)
-        print(np.mean(data[0,:,0]))
-        print(np.mean(data, axis=1))
-        preprocessed_data = data#preprocess(data)
-        encoded_data = encoder.encode(preprocessed_data, encoding_window="full_series")
-        embedding_report_list.append({"embedding": encoded_data[0], "report": row.report})
-    with open(out_path+out_name, "wb") as f:
+        row = dataset[i:i+1] # Get a single row as a dataframe instead of a series
+        data = load_raw_data(row, sampling_rate, data_folder)
+        preprocessed_data = preprocess(data)
+        # Copy is a workaround for the preprocessing putting a negative stride on the nparray
+        encoded_data = encoder.encode(preprocessed_data.copy(), encoding_window="full_series")
+        embedding_report_list.append({"embedding": encoded_data[0], "report": row["report"].array[0], "ecg_id": row["ecg_id"].array[0]})
+    with open(out_folder+out_name, "wb") as f:
         pickle.dump(embedding_report_list, f)
 
 
@@ -68,12 +71,12 @@ for out_name, dataset in [("parsed_ptb_train.pkl", ptb_df_train), ("parsed_ptb_t
 #     embedding_report_list = []
 #     for i in tqdm(range(len(dataset)//n_samples)):
 #         dataset_slice = dataset[i*n_samples:min((i+1)*n_samples, len(dataset))]
-#         data = load_raw_data(dataset_slice, sampling_rate, data_path)
+#         data = load_raw_data(dataset_slice, sampling_rate, data_folder)
 #         preprocessed_data = preprocess(data)
 #         encoded_data = encoder.encode(preprocessed_data, encoding_window="full_series")
 #         for i in range(n_samples):
 #             embedding_report_list.append({"embedding": encoded_data[i], "report": dataset_slice.iloc[i].report})
-#     with open(out_path+out_name, "wb") as f:
+#     with open(out_folder+out_name, "wb") as f:
 #         pickle.dump(embedding_report_list, f)
 
     
