e551a741f1dcde27e8027a63ae2b7d40fa8e4985
Tue Jensen <tuebjensen@gmail.com>
2024-11-14 14:47:41 +0100

    Formatting and add validation loss

--- ws/train_ts2vec.py
+++ ws/train_ts2vec.py
@@ -17,16 +17,16 @@ from utils import data_dropout, init_dl_program, name_with_datetime, pkl_save
 from ts2vec import TS2Vec
 
 
-def save_checkpoint_callback(
-    save_every=1,
-    unit='epoch'
-):
-    assert unit in ('epoch', 'iter')
+def save_checkpoint_callback(save_every=1, unit="epoch"):
+    assert unit in ("epoch", "iter")
+
     def callback(model, loss):
-        n = model.n_epochs if unit == 'epoch' else model.n_iters
+        n = model.n_epochs if unit == "epoch" else model.n_iters
         if n % save_every == 0:
-            model.save(f'{run_dir}/model_{n}.pkl')
+            model.save(f"{run_dir}/model_{n}.pkl")
+
     return callback
+
 
 def apply_highpass_filter(data, lowcut=0.05, sampling_rate=100, axis=-1):
     b, a = scipy.signal.butter(5, lowcut, btype="highpass", fs=sampling_rate)
@@ -51,11 +51,13 @@ def load_raw_data(df, sampling_rate, path):
     ]
     return np.array([signal for signal, meta in data])
 
-def load_ptb_data(data_path='data/ptb-xl/'):
+
+def load_ptb_data(data_path="data/ptb-xl/"):
     sampling_rate = 100
     train_fold_size = 8
     val_fold_size = 1
     ptb_df = pd.read_csv(data_path + "ptbxl_database_translated.csv")
+    ptb_df = ptb_df[0:100]
     # strat_fold goes from 1-10 and is used to split the data into train, validation and test sets
     ptb_df_train = ptb_df[ptb_df.strat_fold <= train_fold_size]
     ptb_df_val = ptb_df[
@@ -63,71 +65,105 @@ def load_ptb_data(data_path='data/ptb-xl/'):
         & (ptb_df.strat_fold <= train_fold_size + val_fold_size)
     ]
     ptb_df_test = ptb_df[train_fold_size + val_fold_size < ptb_df.strat_fold]
-    train_data = load_raw_data(ptb_df_train[0:100], sampling_rate, data_path)
-    return preprocess(train_data, sampling_rate).copy()
+    train_data = load_raw_data(ptb_df_train, sampling_rate, data_path)
+    val_data = load_raw_data(ptb_df_val, sampling_rate, data_path)
+    preprocessed_train_data = preprocess(train_data, sampling_rate)
+    preprocessed_val_data = preprocess(val_data, sampling_rate)
+    return preprocessed_train_data.copy(), preprocessed_val_data.copy()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset', help='The dataset name')
-    parser.add_argument('run_name', help='The folder name used to save model, output and evaluation metrics. This can be set to any word')
-    parser.add_argument('--gpu', type=int, default=0, help='The gpu no. used for training and inference (defaults to 0)')
-    parser.add_argument('--batch-size', type=int, default=8, help='The batch size (defaults to 8)')
-    parser.add_argument('--lr', type=float, default=0.001, help='The learning rate (defaults to 0.001)')
-    parser.add_argument('--repr-dims', type=int, default=320, help='The representation dimension (defaults to 320)')
-    parser.add_argument('--max-train-length', type=int, default=3000, help='For sequence with a length greater than <max_train_length>, it would be cropped into some sequences, each of which has a length less than <max_train_length> (defaults to 3000)')
-    parser.add_argument('--iters', type=int, default=None, help='The number of iterations')
-    parser.add_argument('--epochs', type=int, default=None, help='The number of epochs')
-    parser.add_argument('--save-every', type=int, default=None, help='Save the checkpoint every <save_every> iterations/epochs')
-    parser.add_argument('--seed', type=int, default=None, help='The random seed')
-    parser.add_argument('--max-threads', type=int, default=None, help='The maximum allowed number of threads used by this process')
-    parser.add_argument('--eval', action="store_true", help='Whether to perform evaluation after training')
+    parser.add_argument("dataset", help="The dataset name")
+    parser.add_argument(
+        "run_name",
+        help="The folder name used to save model, output and evaluation metrics. This can be set to any word",
+    )
+    parser.add_argument(
+        "--gpu",
+        type=int,
+        default=0,
+        help="The gpu no. used for training and inference (defaults to 0)",
+    )
+    parser.add_argument(
+        "--batch-size", type=int, default=8, help="The batch size (defaults to 8)"
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.001, help="The learning rate (defaults to 0.001)"
+    )
+    parser.add_argument(
+        "--repr-dims",
+        type=int,
+        default=320,
+        help="The representation dimension (defaults to 320)",
+    )
+    parser.add_argument(
+        "--max-train-length",
+        type=int,
+        default=3000,
+        help="For sequence with a length greater than <max_train_length>, it would be cropped into some sequences, each of which has a length less than <max_train_length> (defaults to 3000)",
+    )
+    parser.add_argument(
+        "--iters", type=int, default=None, help="The number of iterations"
+    )
+    parser.add_argument("--epochs", type=int, default=None, help="The number of epochs")
+    parser.add_argument(
+        "--save-every",
+        type=int,
+        default=None,
+        help="Save the checkpoint every <save_every> iterations/epochs",
+    )
+    parser.add_argument("--seed", type=int, default=None, help="The random seed")
+    parser.add_argument(
+        "--max-threads",
+        type=int,
+        default=None,
+        help="The maximum allowed number of threads used by this process",
+    )
+    parser.add_argument(
+        "--eval",
+        action="store_true",
+        help="Whether to perform evaluation after training",
+    )
     args = parser.parse_args()
-    
+
     print("Dataset:", args.dataset)
     print("Arguments:", str(args))
-    
-    device = init_dl_program(args.gpu, seed=args.seed, max_threads=args.max_threads)
-    
-    print('Loading data... ', end='')
 
-    ptb_path = 'data/ptb-xl/'
-    train_data = load_ptb_data(data_path=ptb_path)
+    # device = init_dl_program(args.gpu, seed=args.seed, max_threads=args.max_threads)
 
-    print('done')
-    
+    print("Loading data... ", end="")
+
+    ptb_path = "data/ptb-xl/"
+    train_data, val_data = load_ptb_data(data_path=ptb_path)
+
+    print("done")
+
     config = dict(
         batch_size=args.batch_size,
         lr=args.lr,
         output_dims=args.repr_dims,
-        max_train_length=args.max_train_length
+        max_train_length=args.max_train_length,
     )
-    
-    if args.save_every is not None:
-        unit = 'epoch' if args.epochs is not None else 'iter'
-        config[f'after_{unit}_callback'] = save_checkpoint_callback(args.save_every, unit)
 
-    run_dir = 'training/' + args.dataset + '__' + name_with_datetime(args.run_name)
+    if args.save_every is not None:
+        unit = "epoch" if args.epochs is not None else "iter"
+        config[f"after_{unit}_callback"] = save_checkpoint_callback(
+            args.save_every, unit
+        )
+
+    run_dir = "training/" + args.dataset + "__" + name_with_datetime(args.run_name)
     os.makedirs(run_dir, exist_ok=True)
-    
+
     t = time.time()
-    
-    model = TS2Vec(
-        input_dims=train_data.shape[-1],
-        device=device,
-        **config
-    )
+
+    model = TS2Vec(input_dims=train_data.shape[-1], device="cpu", **config)
     loss_log = model.fit(
-        train_data,
-        n_epochs=args.epochs,
-        n_iters=args.iters,
-        verbose=True
+        train_data, val_data, n_epochs=args.epochs, n_iters=args.iters, verbose=True
     )
-    model.save(f'{run_dir}/model.pkl')
+    model.save(f"{run_dir}/model.pkl")
 
     t = time.time() - t
     print(f"\nTraining time: {datetime.timedelta(seconds=t)}\n")
-
-
 
     print("Finished.")
