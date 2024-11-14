87e9140be04d0b1bec04f163db7fc2c1d8ba165a
Tamas Halasi <trusted.tomato@gmail.com>
2024-11-14 13:23:11 +0100

    fix bugs in scripts

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ source .env.local
 source init-connection.sh
 
 # Check the existence of used environment variables
-TEMP="$HOME $WS_PATH $USER $HOST $REMOTE_PROJECT_PATH $REMOTE_COMMAND"
+TEMP="$HOME $WS_PATH $USER $HOST $REMOTE_PROJECT_PATH $REMOTE_COMMAND $GROUP"
 
 # Make remote target directory
 ssh -o "ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p" $USER@$HOST mkdir -p $REMOTE_PROJECT_PATH/$WS_PATH
 
 # Sync from local to remote
-rsync -e "ssh -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" -rltvzP --perms --chmod=770 --delete --exclude-from=.rsyncignore $WS_PATH/ $USER@$HOST:$REMOTE_PROJECT_PATH/$WS_PATH
+rsync -e "ssh -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" -rltvzP --perms --chmod=770 --groupmap='*:'"$GROUP" --delete --exclude-from=.rsyncignore $WS_PATH/ $USER@$HOST:$REMOTE_PROJECT_PATH/$WS_PATH
 
 # Run command on remote from $REMOTE_PATH/$WS_PATH
 ssh -o "ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p" $USER@$HOST "cd $REMOTE_PROJECT_PATH/$WS_PATH && $REMOTE_COMMAND $@"
--- ws/main_run.sh
+++ ws/main_run.sh
@@ -6,4 +6,4 @@ if [ ! -d ".venv" ]; then
     python3 -m venv --system-site-packages .venv
 fi
 
-srun --gres=gpu:1 singularity exec --nv /ceph/container/pytorch/pytorch_24.09.sif bash ./main_run.sh "$@"
+srun --gres=gpu:1 singularity exec --nv /ceph/container/pytorch/pytorch_24.09.sif bash ./main.sh "$@"
