03d6310a6c4bd7293a2a225ca549e525924d68ed
Tue Jensen <tuebjensen@gmail.com>
2024-11-19 16:49:19 +0100

    Add option to export metrics as csv

--- ws/TSCapMetrics.py
+++ ws/TSCapMetrics.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 import ptbtok
@@ -28,7 +30,6 @@ class TSCapMetrics:
             ["rouge1", "rouge2", "rougeL"], use_stemmer=False
         )
         self._metrics = {}
-        self._metricsdf = pd.DataFrame()
 
     # Function to calculate BLEU score
     def _calculate_bleu_scores(self, ref_sentences, hyp_sentences, max_n=4):
@@ -47,13 +48,13 @@ class TSCapMetrics:
         )
 
         for i in range(1, max_n + 1):
-            if f"bleu{i}" not in self._metrics:
-                self._metrics[f"bleu{i}"] = []
-            self._metrics[f"bleu{i}"].append(bleu_nltk[i - 1])
+            if f"BLEU-{i}" not in self._metrics:
+                self._metrics[f"BLEU-{i}"] = []
+            self._metrics[f"BLEU-{i}"].append(bleu_nltk[i - 1])
 
-        if "sacrebleu" not in self._metrics:
-            self._metrics["sacrebleu"] = []
-        self._metrics["sacrebleu"].append(bleu_sacre.score / 100)
+        if "sacreBLEU" not in self._metrics:
+            self._metrics["sacreBLEU"] = []
+        self._metrics["sacreBLEU"].append(bleu_sacre.score / 100)
 
     # Function to calculate METEOR score
     def _calculate_meteor_scores(self, ref_sentences, hyp_sentences):
@@ -63,9 +64,9 @@ class TSCapMetrics:
                 self.tokenizer(ref).split(), self.tokenizer(hyp).split()
             )
             meteor_scores.append(meteor)
-        if "meteor" not in self._metrics:
-            self._metrics["meteor"] = []
-        self._metrics["meteor"].append(np.mean(meteor_scores))
+        if "METEOR" not in self._metrics:
+            self._metrics["METEOR"] = []
+        self._metrics["METEOR"].append(np.mean(meteor_scores))
 
     # Function to calculate ROUGE score
     def _calculate_rouge_scores(self, ref_sentences, hyp_sentences):
@@ -78,7 +79,7 @@ class TSCapMetrics:
             rouge2_scores.append(scores["rouge2"].fmeasure)
             rougeL_scores.append(scores["rougeL"].fmeasure)
         for key, values in zip(
-            ["rouge1", "rouge2", "rougeL"],
+            ["ROUGE-1", "ROUGE-2", "ROUGE-L"],
             [rouge1_scores, rouge2_scores, rougeL_scores],
         ):
             if key not in self._metrics:
@@ -105,19 +106,29 @@ class TSCapMetrics:
             tokenizer=ptbtok.tokenize,
             return_all_scores=False,
         )
-        if "cider_d" not in self._metrics:
-            self._metrics["cider_d"] = []
-        self._metrics["cider_d"].append(cider.item())
+        if "CIDEr-D" not in self._metrics:
+            self._metrics["CIDEr-D"] = []
+        self._metrics["CIDEr-D"].append(cider.item())
 
     def _calculate_spice_scores(self, ref_sentences, hyp_sentences, java_path=None):
         ref_sentences_as_singletons = [[sentence] for sentence in ref_sentences]
-        spice_score = spice(hyp_sentences, ref_sentences_as_singletons, java_path=java_path, return_all_scores=False)
-        if "spice" not in self._metrics:
-            self._metrics["spice"] = []
-        self._metrics["spice"].append(spice_score.item())
+        spice_score = spice(
+            hyp_sentences,
+            ref_sentences_as_singletons,
+            java_path=java_path,
+            return_all_scores=False,
+        )
+        if "SPICE" not in self._metrics:
+            self._metrics["SPICE"] = []
+        self._metrics["SPICE"].append(spice_score.item())
 
     def calculate_metrics(
-        self, ref_sentences: list[str], hyp_sentences: list[list[str]], java_path=None
+        self,
+        ref_sentences: list[str],
+        hyp_sentences: list[list[str]],
+        out_dir: str = None,
+        out_name: str = None,
+        java_path: str = None,
     ):
         # hyp_sentences should be a list of list of strings where the dimensions are [n_samples, n_sentences]
         # So you would run predict n_samples amount of times and store the result of predict in a list of size n_sentences
@@ -130,9 +141,11 @@ class TSCapMetrics:
             self._calculate_rouge_scores(ref_sentences, hyp_sentences[i])
             self._calculate_bert_scores(ref_sentences, hyp_sentences[i])
             self._calculate_ciderd_scores(ref_sentences, hyp_sentences[i])
-            #We can't do this without the correct java version
+            # We can't do this without the correct java version
             if java_path:
-                self._calculate_spice_scores(ref_sentences, hyp_sentences[i], java_path=java_path)
+                self._calculate_spice_scores(
+                    ref_sentences, hyp_sentences[i], java_path=java_path
+                )
 
         for key, value in self._metrics.items():
             conf_int = st.t.interval(
@@ -145,6 +158,11 @@ class TSCapMetrics:
                     float(round(conf_int[1], 3)),
                 ),
             }
+        if out_dir and out_name:
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+            metrics_df = pd.DataFrame(metrics_with_conf_int)
+            metrics_df.to_csv(os.path.join(out_dir, out_name), index=False)
 
         return metrics_with_conf_int
 
@@ -152,10 +170,20 @@ class TSCapMetrics:
 if __name__ == "__main__":
     # Example predictions and references
     predictions = [["hello general kenobi, how are you", "foo bar bar bar foobar"]] * 10
-    references = ["hello there general kenobi, how are you", "foo bar bar bar bar foobar"]
-    
+    references = [
+        "hello there general kenobi, how are you",
+        "foo bar bar bar bar foobar",
+    ]
 
     # Initialize the TSCapMetrics class
     ts_cap_metrics = TSCapMetrics()
 
-    print(ts_cap_metrics.calculate_metrics(references, predictions, java_path="~/jdk-11.0.2/bin/java"))
+    print(
+        ts_cap_metrics.calculate_metrics(
+            references,
+            predictions,
+            out_dir="./metrics",
+            out_name="test.csv",
+            java_path="~/jdk-11.0.2/bin/java",
+        )
+    )
