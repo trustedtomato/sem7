d188c11bcb6e9e90f75c52386a152676ab49db9c
Tamas Halasi <trusted.tomato@gmail.com>
2024-11-19 01:10:41 +0100

    and cancellation to main_run.sh to reduce load on aau ailab

--- ws/main_run.sh
+++ ws/main_run.sh
@@ -6,4 +6,7 @@ if [ ! -d ".venv" ]; then
     python3 -m venv .venv
 fi
 
+# Cancel all our jobs before starting a new one
+squeue --me --nohead --format %F | uniq | xargs -I %s -- scancel %s
+
 srun --gres=gpu:6 --mem=60G singularity exec  --nv /ceph/container/pytorch/pytorch_24.09.sif bash ./main.sh "$@"
