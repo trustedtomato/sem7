d4d8896b4d0efcf92852b40454bff79e031e2c7b
Tue Jensen <tuebjensen@gmail.com>
2024-11-23 12:32:01 +0100

    Simplify model initialization

--- ws/train_ts2vec.py
+++ ws/train_ts2vec.py
@@ -46,28 +46,17 @@ def main(args):
     os.makedirs(output_dir, exist_ok=True)
     t = time.time()
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    if not args.use_config:
-        model = TS2Vec(
-            input_dim=train_data.shape[-1],
-            depth=args.tsencoder_depth,
-            hidden_dim=args.tsencoder_hidden_dim,
-            batch_size=config.tsencoder_batch_size,
-            device=device,
-            lr=args.lr,
-            output_dim=args.ts_embedding_dim,
-            max_train_length=args.max_train_length,
-        )
-    else:
-        model = TS2Vec(
-            input_dim=train_data.shape[-1],
-            depth=config.tsencoder_depth,
-            hidden_dim=config.tsencoder_hidden_dim,
-            batch_size=config.tsencoder_batch_size,
-            device=device,
-            lr=args.lr,
-            output_dim=config.ts_embedding_dim,
-            max_train_length=args.max_train_length,
-        )
+    model = TS2Vec(
+        input_dim=train_data.shape[-1],
+        depth=args.tsencoder_depth,
+        hidden_dim=args.tsencoder_hidden_dim,
+        batch_size=config.tsencoder_batch_size,
+        device=device,
+        lr=args.lr,
+        output_dim=args.ts_embedding_dim,
+        max_train_length=args.max_train_length,
+    )
+
     # This saves the model on it's own too when it's done training
     model.fit(
         train_data,
@@ -122,7 +111,6 @@ if __name__ == "__main__":
         default=None,
         help="Save the checkpoint every <save_every> iterations/epochs",
     )
-    parser.add_argument("--use_config", dest="use_config", action="store_true")
     parser.add_argument(
         "--tsencoder_hidden_dim",
         type=int,
