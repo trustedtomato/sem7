5b7f955e269a24c1fdc035758652e244a65506d5
Tue Jensen <tuebjensen@gmail.com>
2024-12-19 16:49:45 +0100

    Add minor changes to code

--- ws/ail_fe_ts2vec_scmds.py
+++ ws/ail_fe_ts2vec_scmds.py
@@ -13,12 +13,11 @@ def modify_parser(parser: argparse._ArgumentGroup):
     )
 
 
-def get_scmd(experiment):
+def get_scmd(experiment, folder_name):
     hd = experiment["tsencoder_hidden_dim"]
     d = experiment["tsencoder_depth"]
     ed = experiment["ts_embedding_dim"]
-    epochs = experiment.get("epochs", 1000)
-    folder_name = experiment["folder_name"]
+    epochs = experiment["epochs"]
     name = f"ts2vec_hd{hd}_d{d}_ed{ed}_ep{epochs}"
     return SCmd(
         program="srun",
@@ -46,6 +45,5 @@ def get_scmds(args: argparse.Namespace):
         experiments_dict = json.load(f)
         folder_name = experiments_dict["folder_name"]
         experiments = experiments_dict["experiments"]
-        for experiment in experiments:
-            experiment["folder_name"] = folder_name
-    return [get_scmd(experiment) for experiment in experiments]
+
+    return [get_scmd(experiment, folder_name) for experiment in experiments]
